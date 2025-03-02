a76777623ba30dc1b2e1fde2b0668ffc8cccf3c1
vera_chernevich <9111402516v@gmail.com>
2025-03-02 16:39:00 +0300

    тренировка

---
+++ src/Task19/ListNode.java
@@ -0,0 +1,7 @@
+package Task19;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
---
+++ src/Task19/Solution.java
@@ -0,0 +1,25 @@
+package Task19;
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast pointer n steps ahead
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both pointers until fast reaches the end
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // Remove the nth node from the end
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
+
